02bafc7ab1f137ae194547168b7c1dad9d9c3d1d
godslew <maousnake00@live.jp>
2024-05-08 14:08:03 +0900

    disable pip

--- app/src/main/java/com/godslew/tlaloc/MainActivity.kt
+++ app/src/main/java/com/godslew/tlaloc/MainActivity.kt
@@ -1,43 +1,65 @@
 package com.godslew.tlaloc
 
-import android.app.PictureInPictureParams
-import android.os.Build
 import android.os.Bundle
-import android.util.Rational
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.godslew.tlaloc.designsystem.theme.TlalocTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setupPip()
 
     setContent {
       TlalocTheme {
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
         // A surface container using the 'background' color from the theme
         Scaffold(
           modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+          topBar = {
+            TopAppBar(
+              modifier = Modifier
+                .statusBarsPadding(),
+              title = {
+                Text(
+                  text = "Tlaloc",
+                )
+              },
+              colors = TopAppBarDefaults.topAppBarColors().copy(
+                containerColor = Color.Transparent,
+                scrolledContainerColor = Color.Transparent,
+              ),
+              windowInsets = WindowInsets(0.dp),
+              scrollBehavior = scrollBehavior,
+            )
+          },
+          contentWindowInsets = WindowInsets(0.dp)
         ) {
           LazyColumn(
             modifier = Modifier
@@ -55,17 +77,6 @@ class MainActivity : ComponentActivity() {
           }
         }
       }
-    }
-  }
-
-  private fun setupPip() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-      setPictureInPictureParams(
-        PictureInPictureParams.Builder()
-          .setAspectRatio(Rational(9, 16))
-          .setAutoEnterEnabled(true)
-          .build()
-      )
     }
   }
 }
